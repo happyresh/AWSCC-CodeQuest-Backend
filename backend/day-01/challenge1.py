0c3cf64141c7187286d0a148fcb140abb466e4d1
Recio Eliniel <elinielvrecio@iskolarngbayan.pup.edu.ph>
2023-11-22 00:13:28 +0800

    Day 1 Challenges

---
+++ backend/day-01/challenge1.py
@@ -0,0 +1,7 @@
+# Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!
+# One thing about me is that {about_you}.
+your_name = "Eliniel"
+name_of_department = "Department of Software and Web Development"
+about_you = "I love being able to learn new things and utilize it in challenges like this!"
+
+print(f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!\nOne thing about me is that {about_you}")
--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = None
-age = None
-isMale = None
-department = None
+name = 'Eliniel'
+age = "19"
+isMale = "Yes"
+department = "Frontend"
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
